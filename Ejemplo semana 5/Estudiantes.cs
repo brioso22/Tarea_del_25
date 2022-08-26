17d233f9ed960060ba94fb768eb131cd2a98dda3
Marcos Brioso <marcosbriosobenavides2002@gmail.com>
2022-08-25 21:15:44 -0600

    tarea del 25

---
+++ Ejemplo semana 5/Estudiantes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c
+{
+
+    class Estudiantes
+    {
+        private int id;
+        private String código;
+        private String nombre;
+        private int edad;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String Código
+        {
+            get { return código; }
+            set { código = value; }
+        }
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+
+
+        public Estudiantes(int id, String código, string nombre, int edad)
+        {
+            this.id = id;
+            this.código = código;
+            this.nombre = nombre;
+            this.edad = edad;
+
+        }
+
+        public string mostrar()
+        {
+            return id + " " + código + " " + nombre + " " + edad;
+        }
+    }
+
+}
---
+++ Ejemplo semana 5/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id;
+            String Código;
+            String Nombre;
+            int Edad;
+
+            Console.WriteLine("Cuantos registros desea agregar:");
+            int registros = int.Parse(Console.ReadLine());
+
+            Estudiantes[] arregloestudiantes = new Estudiantes[registros];
+
+
+            for (int i = 0; i < registros; i++)
+            {
+                Id = i + 1;
+                Console.WriteLine("Ingrese los siguientes datos:");
+                Console.WriteLine("Ingrese el Código:");
+                Código = Console.ReadLine();
+                Console.WriteLine("Ingrese el Nombre:");
+                Nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese la edad:");
+                Edad = int.Parse(Console.ReadLine());
+
+                arregloestudiantes[i] = new Estudiantes(Id, Código, Nombre, Edad);
+            }
+
+            // REALIZAR CONSULTAS
+            int con = 1;
+            
+            while (con == 1)
+            {
+                int datoint,op;
+                string dato;
+
+                Console.WriteLine(" por favor digite 1 si decea hacer una cosulta por edad, 2 si decea hacerla id y 3 si la decea por nombre ");
+                op = int.Parse(Console.ReadLine());
+                if (op == 1)
+                {
+
+                    Console.WriteLine("digite apartir de que rango decea buscar la edad ");
+                    datoint = int.Parse(Console.ReadLine());
+
+                    IEnumerable<Estudiantes> consultaestudiantes = from estudiante in arregloestudiantes
+                                                                   where estudiante.Edad >= datoint
+                                                                   select estudiante;
+                    Console.WriteLine("El resultado de la consulta es:");
+                    foreach (Estudiantes consulta in consultaestudiantes)
+                    {
+                        Console.WriteLine(consulta.mostrar());
+
+                    }
+
+                }
+
+                else if (op == 2)
+                {
+
+                    Console.WriteLine("digite el id que busca ");
+                    datoint = int.Parse(Console.ReadLine());
+
+                    IEnumerable<Estudiantes> consultaestudiantes = from estudiante in arregloestudiantes
+                                                                   where estudiante.Id == datoint
+                                                                   select estudiante;
+                    Console.WriteLine("El resultado de la consulta es:");
+                    foreach (Estudiantes consulta in consultaestudiantes)
+                    {
+                        Console.WriteLine(consulta.mostrar());
+
+                    }
+                }
+                else if (op == 3)
+                {
+
+
+                    Console.WriteLine("digite apartir de que rango decea buscar la edad ");
+                    dato = Console.ReadLine();
+
+                    IEnumerable<Estudiantes> consultaestudiantes = from estudiante in arregloestudiantes
+                                                                   where estudiante.Nombre == dato
+                                                                   select estudiante;
+                    Console.WriteLine("El resultado de la consulta es:");
+                    foreach (Estudiantes consulta in consultaestudiantes)
+                    {
+                        Console.WriteLine(consulta.mostrar());
+
+                    }
+
+
+                }
+                Console.WriteLine("digite 1 si decea consultar nuevamente y cualquier otro numero si no ");
+                con = int.Parse(Console.ReadLine());
+
+            }
+
+
+            
+            Console.Read();
+
+        }
+        
+    }    
+           
+}
